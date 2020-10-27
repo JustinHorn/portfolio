e205fc9f5bc0e17b27809da43285ca75fb2e2245
y0092888 <ju-horn@web.de>
2020-10-27 15:50:32 +0100

    fixed hr color

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,16 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-import Link from "next/Link";
-
 import Profile from "component/Profile";
 
-import LinkIcon from "component/Icon/LinkIcon";
-
 import { footer } from "data";
+
+import styles from "styles/home.module.css";
 
 export default function Home() {
   const { icons } = footer;
 
   return (
-    <div className="contrast">
+    <div className={"contrast " + styles.page}>
       <h1 className={styles.headline}>Hi, welcome to my Portfolio!</h1>
       <hr />
-
       <Profile />
     </div>
   );
