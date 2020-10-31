c18d47dad87366b3b59f642b53a163f80147315e
y0092888 <ju-horn@web.de>
2020-10-31 20:15:36 +0100

    having an idex page again

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/welcome");
+  }, []);
+  return <div className="contrast"></div>;
+};
+
+export default index;
