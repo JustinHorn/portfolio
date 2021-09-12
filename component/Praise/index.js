3f26e3499821418e3fa11810bbbca1d5d205d840
Justin Horn <justinhorn0000@gmail.com>
2021-09-12 17:03:57 +0200

    properly positioned gradient

--- component/Praise/index.js
+++ component/Praise/index.js
@@ -1,32 +1,33 @@
 export const Praise = () => {
   return (
     <section id="praise" className="praise">
-      <p>
+      {/* <p>
         I received the following praise from a previous college and from my
         tutor at the WBS Coding School:
-      </p>
+      </p> */}
 
-      <p>
+      <div>
         <i>
           "Justin has an amazing skill to deconstruct a tough coding challenge
-          into granular pieces, and thereafter re-using the latter pieces in the
-          most efficient manner to build out a modern web architecture. [...]"
+          into granular pieces, [...]"
         </i>
-        - Dirk Bosman - Data Scientist
-      </p>
-      <p>
+        <h4> Dirk Bosman </h4>
+        <h5>Data Scientist</h5>
+      </div>
+      <div>
         <i>
           "Justin is an exceptionally capable coder who not only has a deep
           understanding of design patterns and a solid grasp on architecture
           fundamentals, but also can translate high-level concepts into robust,
           functional code. [...]"
         </i>
-        - Federica Recatiani Tutor at the WBS Coding School
-      </p>
-      <p>
+        <h4>Federica Recatiani</h4>
+        <h5>Senior Developer and Tutor at the WBS Coding School </h5>
+      </div>
+      {/* <p>
         Futhermore 16 people have confirmed my skill in React on Linkedin and 14
         my skill in nodejs.
-      </p>
+      </p> */}
     </section>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,25 @@ import { Praise } from "component/Praise";
 import Projects from "component/Projects";
 import { Technology } from "component/Technology";
 import Head from "next/head";
+import { useRef, useEffect, useState } from "react";
 
 config.autoAddCss = false;
 library.add(fas, fab);
 
 function Main() {
+  const innerRef = useRef();
+  const mainRef = useRef();
+
+  const [height, setHeight] = useState("");
+
+  useEffect(() => {
+    const { top: topHeadline, height: headlineHeight } =
+      innerRef.current.getBoundingClientRect();
+    const topMain = mainRef.current.getBoundingClientRect().top;
+
+    setHeight(topHeadline - topMain + Math.floor(headlineHeight / 2) + "px");
+  }, []);
+
   return (
     <div className="PortfolioView">
       <Head>
@@ -20,10 +34,22 @@ function Main() {
       </Head>
       <Nav />
       <Header />
-      <main id="main">
+      <main
+        id="main"
+        ref={mainRef}
+        style={{
+          backgroundImage: `radial-gradient(
+    circle at 50% ${height},
+    black 0,
+    white 10%,
+    grey 20%,
+    black 70%
+  )`,
+        }}
+      >
         <Technology />
         <Praise />
-        <JoyOfCode />
+        <JoyOfCode headlineRef={innerRef} />
         <Projects />
         <section id="about" className="about">
           <p>
